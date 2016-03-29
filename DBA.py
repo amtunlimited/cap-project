37a005733059dc7d1a33c620ce1c5d60261af515
ChrisDumond <chrisdumond96@gmail.com>
2016-03-29 10:11:47 -0500

    Add getAllItems() function

--- DBA.py
+++ DBA.py
@@ -6,14 +6,18 @@ db = web.database(dbn = 'sqlite', db = 'CAP')
 def addItem(productNumber, description, price, count, threshold, taxable, cost):
 	db.insert('Inventory', ProductNumber=productNumber, Description=description, Price=price, Count=count, Threshold=threshold, Taxable=taxable, Cost=cost)
 
-#get the item with the given product number
+#get the item from inventory with the given product number
 def getItem(productNumber):
 	try:
 		return db.select('Inventory', where='ProductNumber=$productNumber')[0]
 	except IndexError:
 		return None
 
-#update count field server-side		
+#get all items from inventory, ordered by product number
+def getAllItems():
+	return db.select('Inventory', order = 'ProductNumber')
+
+#update count field server-side
 def updateCount(productNumber, delta):
 	db.query(
 		'UPDATE Inventory \
