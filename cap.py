aeac8b816b4cda2b7bb3d66f114e67f65299851c
Phillip Peterson <phillipdpeterson@gmail.com>
2016-04-18 02:12:18 -0500

    added necessary methods for inventory

--- cap.py
+++ cap.py
@@ -12,6 +12,15 @@ urls = (
 	'/', 'index',
 	'/login/', 'login',
 	'/logout/', 'logout',
+	'/inventory/', 'inventory',
+	'/updateDescription/', 'updateDescription',
+	'/updatePrice/', 'updatePrice',
+	'/updateCost/', 'updateCost',
+	'/updateThreshold/', 'updateThreshold',
+	'/updateCount/', 'updateCount',
+	'/updateTax/', 'updateTax',
+	'/deleteItem/', 'deleteItem',
+	'/addItem/', 'addItem',
 )
 
 app = web.application(urls, globals())
@@ -21,8 +30,71 @@ session = web.session.Session(app, web.session.DiskStore('sessions'), initialize
 class item:
 	def POST(self):
 		ops = json.loads(web.data())
-		web.header('Content-Type', 'application/json')
-		return json.dumps(DBA.getItem(web.data()))
+		if web.data() == "-1":
+			web.header('Content-Type', 'application/json')
+			return json.dumps(list(DBA.getAllItems()))
+		else:
+			web.header('Content-Type', 'application/json')
+			return json.dumps(DBA.getItem(web.data()))
+
+class deleteItem:
+	def POST(self):
+		DBA.removeItem(web.data())
+
+class addItem:
+	def POST(self):
+		DBA.addItem("", 0, 0, 0, 1, 0)
+		raise web.seeother('/')
+
+class updateDescription:
+	def POST(self):
+		str = web.data()
+		list = str.split(' ', 1)
+		
+		DBA.updateDescription(list[0], list[1])
+
+class updatePrice:
+	def POST(self):
+		str = web.data()
+		list = str.split(' ', 1)
+		
+		DBA.updatePrice(list[0], list[1])
+
+class updateCost:
+	def POST(self):
+		str = web.data()
+		list = str.split(' ', 1)
+		
+		DBA.updateCost(list[0], list[1])
+
+class updateThreshold:
+	def POST(self):
+		str = web.data()
+		list = str.split(' ', 1)
+		
+		DBA.updateThreshold(list[0], list[1])
+
+class updateCount:
+	def POST(self):
+		str = web.data()
+		list = str.split(' ', 1)
+		
+		DBA.updateCount(list[0], list[1])
+
+class updateTax:
+	def POST(self):
+		str = web.data()
+		list = str.split(' ', 1)
+		
+		DBA.updateTaxable(list[0], list[1])
+
+class inventory:
+	def GET(self):
+		if(session.user!=1):
+			raise web.seeother('/login/')
+
+		index = web.template.frender('inventory.html')
+		return index()
 
 class index:
 	def GET(self):
