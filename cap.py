da5090c4258da7f1ce1cbae84578ff1f221773f9
Aaron Tagliaboschi <arn44621@aarontag.com>
2016-04-17 23:09:59 -0400

    log in test

--- cap.py
+++ cap.py
@@ -4,10 +4,18 @@ import web
 import json
 import DBA
 
+web.config.debug = False
+
 urls = (
 	'/item/', 'item',
-	'/', 'index'
+	'/', 'index',
+	'/login/', 'login',
+	'/logout/', 'logout',
 )
+
+app = web.application(urls, globals())
+session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'user': 0})
+
 
 class item:
 	def POST(self):
@@ -17,9 +25,28 @@ class item:
 
 class index:
 	def GET(self):
+		if(session.user==0):
+			raise web.seeother('/login/')
+
 		index = web.template.frender('index.html')
 		return index()
 
+class login:
+	def GET(self):
+		login = web.template.frender('login.html')
+		return login()
+	
+	def POST(self):
+		if(web.input().user=="root"):
+			session.user=1
+			raise web.seeother('/')
+		else:
+			raise web.seeother('/login/')
+			
+class logout:
+	def GET(self):
+		session.kill()
+		raise web.seeother('/login/')
+
 if __name__ == "__main__":
-	app = web.application(urls, globals())
 	app.run()
